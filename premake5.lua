acbb3c13ca9a7025822fa80aaf45858b2067c02b
AdrianNguyen-UIT <18520955@gm.uit.edu.vn>
2020-04-13 23:08:02 +0700

    Modify .gitignore and premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -2,27 +2,32 @@ project "ImGui"
     kind "StaticLib"
     language "C++"
 
-    targetdir ("bin/"..outputdir.."%{prj.name}")
-    objdir ("obj/"..outputdir.."%{prj.name}")
+    targetdir ("bin/"..outputdir.."/%{prj.name}")
+    objdir ("obj/"..outputdir.."/%{prj.name}")
 
     files
-    {
-        "imconfig.h",
-        "imgui.h",
-        "imgui.cpp",
-        "imgui_draw.cpp",
-        "imgui_internal.h",
-        "imgui_widgets.cpp",
-        "imstb_rectpack.h",
-        "imstb_textedit.h",
-        "imstb_truetype.h",
-        "imgui_demo.cpp"
-    }
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
 
-    filters "system:windows"
+    filter "system:windows"
         systemversion "latest"
         cppdialect "C++17"
-        staticruntime "On"
+		staticruntime "on"
+		
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
 
-    filters {"system:windows" , "configurations:Release"}
-        buildoptions "/MT"
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
